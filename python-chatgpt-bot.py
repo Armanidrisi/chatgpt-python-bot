a3d33660a7c997b7349c03ce5dd7cb272de2bd8c
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2023-03-07 16:45:00 +0530

    Update python-chatgpt-bot.py

--- python-chatgpt-bot.py
+++ python-chatgpt-bot.py
@@ -5,9 +5,9 @@ import openai
 
 
 #Bot Api Token
-API_TOKEN = ''
+API_TOKEN = '6174746961:AAHjTana1jmE-iHiarYPXQeAuu5Llzmsxxw'
 #Openai Api Key
-openai.api_key=""
+openai.api_key="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"
 
 
 bot = telebot.TeleBot(API_TOKEN)
